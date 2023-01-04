894b38e56e6dd4f4d1e2981aeb5bf8e562ea491b
JanelleG51 <janelle_macmillan@yahoo.co.uk>
2023-01-04 23:57:21 +0000

    add: comment code

--- src/deck.py
+++ src/deck.py
@@ -1,6 +1,13 @@
 from src.card import *
 
 class Deck:
+    """
+    Creates a deck of cards from the imported Card class and variables.
+    The deck class starts with an empty card array then loops through
+    the fours suits and fourteen cards in the face and suits variables adding them to the empty array when instantiated. 
+    This totals a number of 52 cards and does not take card value into 
+    account - unit test passes.  
+    """
     def __init__(self):
         self.cards = []
         for suit in suits:
