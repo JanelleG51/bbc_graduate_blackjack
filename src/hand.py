caa78c589336c28918a22cb107441c074d471b68
JanelleG51 <janelle_macmillan@yahoo.co.uk>
2023-01-04 23:09:35 +0000

    feat: create Hand class and deal_card funtion

---
+++ src/hand.py
@@ -0,0 +1,10 @@
+from src.card import *
+
+class Hand:
+    def __init__(self):
+        self.cards = []
+        self.value = 0
+
+    def deal_card(self, card):
+        self.cards.append(card)
+        self.value +=values[card.face]
