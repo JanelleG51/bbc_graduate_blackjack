ad38fbd014c03da46ceb65a9efee26a5cc0d6f06
JanelleG51 <janelle_macmillan@yahoo.co.uk>
2023-01-04 23:56:34 +0000

    add: comment code

--- src/card.py
+++ src/card.py
@@ -1,12 +1,8 @@
-import random
-
-suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
-faces = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
-values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7,'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10,
-          'Queen':10, 'King':10, 'Ace':11}
-
-
 class Card:
+    """
+    Provides a single instance of a card based on suit and face, and 
+    is the basis for building the required deck of cards. 
+    """
     def __init__(self, suit, face):
         self.suit = suit
         self.face = face
@@ -14,3 +10,7 @@ class Card:
     def __str__(self):
         return self.face + ' of ' + self.suit
 
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+faces = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7,'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10,
+          'Queen':10, 'King':10, 'Ace':11}
