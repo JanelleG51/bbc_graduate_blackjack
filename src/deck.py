5b3607c7770c14a0b752d3978a0786c4c611497d
JanelleG51 <janelle_macmillan@yahoo.co.uk>
2023-01-05 16:56:05 +0000

    test: write failing test for player_dealt_two_cards at start of game

--- src/deck.py
+++ src/deck.py
@@ -4,10 +4,9 @@ from src.card import *
 class Deck:
     """
     Creates a deck of cards from the imported Card class and variables.
-    The deck class starts with an empty card array then loops through
-    the fours suits and fourteen cards in the face and suits variables. This builds Card objects and adds them to the empty array when instantiated. 
-    This totals a number of 52 cards and does not take card value into 
-    account - unit test passes.  
+    The deck class starts with an empty card list then loops through
+    the fours suits and fourteen cards in the face and suits variables. This builds Card objects and adds them to the empty list when instantiated. 
+    This totals a number of 52 cards and does not take card value into account - unit test passes.  
     """
     def __init__(self):
         self.cards = []
@@ -16,15 +15,24 @@ class Deck:
                 self.cards.append(Card(suit,face))
 
     def __str__(self):
+        """
+        Provides the print string for each card.
+        """
         deck_build = ''
         for card in self.deck:
             deck_build += '\n'+card.__str__() 
         return 'The deck has:' + deck_build
 
     def shuffle(self):
+        """
+        Shuffles the deck of 52 cards using the built in random function.
+        """
         random.shuffle(self.deck)
 
     def deal(self):
+        """
+        When the deal function is called, one card is removed from the deck using the pop function ready to be added to the empty cards list stored in the Hand class.
+        """
         deal_one_card = self.deck.pop()
         return deal_one_card
 
--- test/test_deck.py
+++ test/test_deck.py
@@ -1,11 +1,13 @@
 import unittest
 from src.deck import Deck
+from src.hand import Hand
 
 
 class DeckTestCase(unittest.TestCase):
 
     def setUp(self):  # this method will be run before each test
         self.deck = Deck()
+        self.hand = Hand()
 
     def tearDown(self):  # this method will be run after each tests
         pass
@@ -13,6 +15,10 @@ class DeckTestCase(unittest.TestCase):
     def test_number_of_cards(self):  # any method beginning with 'test' will be run by unittest
         number_of_cards = len(self.deck.cards)
         self.assertEqual(number_of_cards, 52)
+
+    def test_player_dealt_two_cards(self):
+        player_dealt_two_cards = len(self.hand.cards)
+        self.assertEqual(player_dealt_two_cards, 2)
 
 
 if __name__ == '__main__':
