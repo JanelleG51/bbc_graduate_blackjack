45ea82258dbbbc41b027608640207dbb47260b9d
JanelleG51 <janelle_macmillan@yahoo.co.uk>
2023-01-08 19:48:29 +0000

    refac: comment code and delete redundant files

--- src/game_functions.py
+++ src/game_functions.py
@@ -3,13 +3,16 @@
 playing = True
 
 def show_hand(player):
+    """
+    Displays the player's hand in the terminal as strings and shows the total value of the cards
+    """
     print("\nPlayer's hand:", *player.cards, sep='\n')
     print("Player's hand =", player.value)
 
 
 def hit(deck, hand):
     """
-    When a player 'hits', another card is popped from the deck, added to their hand and the value updated.
+    When a player 'hits', one card is popped from the deck, added to their existing hand and the value updated. If the value of the card will take the hand over 21 and there is an ace in hand, the score will be adjusted to keep the player under 21.
     """
     hand.deal_card(deck.deal())
     hand.aces_high_low()
@@ -18,8 +21,12 @@ def hit(deck, hand):
 def hit_or_stand(deck, hand):
     global playing
 
+    """
+    While that game is playing and the player's hand remains under 21, they player will have the option to 'hit' until they win or bust, or stand.
+    """
+
     while True:
-        x = input("Would you like to Hit or Stand? Enter 'h' or 's' ")
+        x = input("You're still under 21! Would you like to Hit or Stand? Enter 'h' or 's' ")
 
         if x[0].lower() == 'h':
             hit(deck, hand)
--- src/hand.py
+++ src/hand.py
@@ -2,11 +2,17 @@ from src.card import *
 
 class Hand:
     def __init__(self):
+        """
+        Creates and empty list for the players hand, sets the value of the hand to 0 and the number of aces to 0. Aces will be counted as two values depending on the value of the hand and we need to know how many aces are in hand.
+        """
         self.cards = []
         self.value = 0
         self.aces = 0
 
     def deal_card(self, card):
+        """
+        Appends a single instance of a card to the cards list and adds the value of all cards. If an ace is present, the number of aces in the hand are added to self.aces.
+        """
         self.cards.append(card)
         self.value += values[card.face]
         
@@ -14,6 +20,9 @@ class Hand:
             self.aces += 1
 
     def aces_high_low(self):
+        """
+        If the total value of the hand is over 21, aces are then scored as 1.
+        """
         while self.value > 21 and self.aces:
             self.value -= 10
             self.aces -= 1
