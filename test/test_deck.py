8b27eac550b1930ed3283aa836f026aa5fa2f327
JanelleG51 <janelle_macmillan@yahoo.co.uk>
2023-01-08 18:08:07 +0000

    test: add unit tests for scores of 21 adjusting for aces

--- test/test_deck.py
+++ test/test_deck.py
@@ -1,6 +1,7 @@
 import unittest
 from src.deck import Deck
 from src.hand import Hand
+from src.card import *
 from src.game_functions import *
 
 
@@ -28,9 +29,36 @@ class DeckTestCase(unittest.TestCase):
 
     def test_one_card_dealt_on_hit(self):
         """ Does the players hand update with one card on hit? """
-        one_card_dealt_on_hit = hit(self.deck, self.hand)
         self.hand.deal_card(self.deck.deal())
         self.assertTrue(self.hand.deal_card, 1)
+
+    def test_for_score_of_21_with_king_and_ace(self):
+        card_one = Card("Hearts", "King")
+        card_two = Card("Diamonds", "Ace")
+        self.hand.deal_card(card_one)
+        self.hand.deal_card(card_two)
+        self.assertEqual(self.hand.value, 21)
+
+    def test_for_score_of_21_with_king_queen_ace(self):
+        card_one = Card("Hearts", "King")
+        card_two = Card("Diamonds", "Ace")
+        card_three = Card("Spades", "Queen")
+        self.hand.deal_card(card_one)
+        self.hand.deal_card(card_two)
+        self.hand.deal_card(card_three)
+        self.hand.aces_high_low()
+        self.assertEqual(self.hand.value, 21)
+
+    def test_two_aces_and_nine_score_21(self):
+        card_one = Card("Hearts", "Ace")
+        card_two = Card("Diamonds", "Ace")
+        card_three = Card("Spades", "Nine")
+        self.hand.deal_card(card_one)
+        self.hand.deal_card(card_two)
+        self.hand.deal_card(card_three)
+        self.hand.aces_high_low()
+        self.assertEqual(self.hand.value, 21)
+
 
 
 
